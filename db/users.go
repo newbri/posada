1fa560ed8cf922ba0b1d6aad3f8d34b85d361fc0
Andy Newball <andynewball@gmail.com>
2023-11-09 22:25:49 -0500

    creating connection with db

--- db/users.go
+++ db/users.go
@@ -1,1 +1,35 @@
 package db
+
+import "context"
+
+const createUser = `
+INSERT INTO users (username, hashed_password, full_name, email) 
+VALUES ($1,$2,$3,$4)
+RETURNING username,hashed_password,full_name,email,password_changed_at,created_at
+`
+
+type CreateUserParams struct {
+	Username       string `json:"username"`
+	HashedPassword string `json:"hashed_password"`
+	FullName       string `json:"full_name"`
+	Email          string `json:"email"`
+}
+
+func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, createUser,
+		arg.Username,
+		arg.HashedPassword,
+		arg.FullName,
+		arg.Email,
+	)
+	var user User
+	err := row.Scan(
+		&user.Username,
+		&user.HashedPassword,
+		&user.FullName,
+		&user.Email,
+		&user.PasswordChangedAt,
+		&user.CreatedAt,
+	)
+	return user, err
+}
