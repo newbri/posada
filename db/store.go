28aef2c0c92c1d66c3201eaa796eb8f8be825ba0
Andy Newball <andynewball@gmail.com>
2023-11-09 22:24:29 -0500

    creating interface for Store

--- db/store.go
+++ db/store.go
@@ -1,1 +1,15 @@
 package db
+
+import "database/sql"
+
+type Store interface {
+	Querier
+}
+
+type SQLStore struct {
+	*Queries
+}
+
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{Queries: New(db)}
+}
