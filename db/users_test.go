aceb2af5476e04aa7ccf8a5437abaa5905f0edbf
Andy Newball <andynewball@gmail.com>
2023-11-09 22:27:44 -0500

    Creating first test

--- db/users_test.go
+++ db/users_test.go
@@ -1,1 +1,38 @@
 package db
+
+import (
+	"context"
+	"github.com/newbri/posadamissportia/db/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func createRandomUser(t *testing.T) User {
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
+	arg := CreateUserParams{
+		Username:       util.RandomOwner(),
+		HashedPassword: hashedPassword,
+		FullName:       util.RandomOwner(),
+		Email:          util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.FullName, user.FullName)
+	require.Equal(t, arg.Email, user.Email)
+
+	require.NotZero(t, user.CreatedAt)
+	require.NotZero(t, user.PasswordChangedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
+}
