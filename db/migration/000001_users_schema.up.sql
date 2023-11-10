e3cddd6fb164d01336451fd45900e14e3a72e8ba
Andy Newball <andynewball@gmail.com>
2023-11-09 22:22:04 -0500

    initial migration file for user

--- db/migration/000001_users_schema.up.sql
+++ db/migration/000001_users_schema.up.sql
@@ -4,6 +4,6 @@ CREATE TABLE "users"
     "hashed_password"     text        NOT NULL,
     "full_name"           text        NOT NULL,
     "email"               text UNIQUE NOT NULL,
-    "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00+00',
+    "password_changed_at" timestamptz NOT NULL DEFAULT (now()),
     "created_at"          timestamptz NOT NULL DEFAULT (now())
 );
