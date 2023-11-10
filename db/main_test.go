052ccc01f2c3497f74aa9f99329a14aba1a14cce
Andy Newball <andynewball@gmail.com>
2023-11-09 22:20:50 -0500

    creating main test

--- db/main_test.go
+++ db/main_test.go
@@ -1,1 +1,27 @@
 package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/newbri/posadamissportia/db/util"
+	"log"
+	"os"
+	"testing"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("could not connect to the database", err)
+	}
+
+	testQueries = New(testDB)
+	os.Exit(m.Run())
+}
