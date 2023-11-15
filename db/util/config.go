ddb5121fdd9ee610ff125c14f8d03c3935e902fb
Andy Newball <andynewball@gmail.com>
2023-11-14 22:10:44 -0500

    adding property to struct

--- db/util/config.go
+++ db/util/config.go
@@ -6,6 +6,7 @@ type Config struct {
 	DBDriver          string `mapstructure:"DB_DRIVER"`
 	DBSource          string `mapstructure:"DB_SOURCE"`
 	HTTPServerAddress string `mapstructure:"HTTP_SERVER_ADDRESS"`
+	MigrationURL      string `mapstructure:"MIGRATION_URL"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
