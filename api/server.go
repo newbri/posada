a57001fec741e022ae699f808b817573e6ff2b0c
Andy Newball <andynewball@gmail.com>
2023-11-14 22:11:40 -0500

    adding server configuration code

--- api/server.go
+++ api/server.go
@@ -1,1 +1,38 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/newbri/posadamissportia/db"
+	"github.com/newbri/posadamissportia/db/util"
+)
+
+type Server struct {
+	config util.Config
+	store  db.Store
+	router *gin.Engine
+}
+
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	server := &Server{store: store, config: config}
+
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	// add routes to router
+	router.POST("/users", server.createUser)
+
+	server.router = router
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
