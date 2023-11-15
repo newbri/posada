de8b506417f6705c783c5d2ce800b71a88a35b48
Andy Newball <andynewball@gmail.com>
2023-11-14 22:08:11 -0500

    adding configuration for db and router

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"errors"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+	"github.com/newbri/posadamissportia/api"
+	"github.com/newbri/posadamissportia/db"
+	"github.com/newbri/posadamissportia/db/util"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
-	fmt.Println("Hello Andy")
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal().Msg("cannot load config")
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal().Msg("could not connect to the database")
+	}
+
+	runDBMigration(config.MigrationURL, config.DBSource)
+
+	store := db.NewStore(conn)
+
+	runGinServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal().Msg("cannot create new migrate instance")
+	}
+
+	if err := migration.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatal().Msg("failed to run migration up")
+	}
+
+	log.Info().Msg("db migration successfully")
+}
+
+func runGinServer(config util.Config, store db.Store) {
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal().Msg("cannot create server")
+	}
+
+	err = server.Start(config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal().Msg("cannot start server")
+	}
 }
