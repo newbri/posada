d3ca8065f294566e1e3f2383820211feaea30983
Andy Newball <andynewball@gmail.com>
2023-11-21 11:05:05 -0500

    implementing Update User

--- db/querier.go
+++ db/querier.go
@@ -5,4 +5,5 @@ import "context"
 type Querier interface {
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
 	GetUser(ctx context.Context, username string) (User, error)
+	UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error)
 }
--- db/users.go
+++ db/users.go
@@ -1,6 +1,9 @@
 package db
 
-import "context"
+import (
+	"context"
+	"database/sql"
+)
 
 const createUser = `
 INSERT INTO users (username, hashed_password, full_name, email) 
@@ -41,6 +44,45 @@ FROM users WHERE username = $1;
 
 func (q *Queries) GetUser(ctx context.Context, username string) (User, error) {
 	row := q.db.QueryRowContext(ctx, getUser, username)
+	var user User
+	err := row.Scan(
+		&user.Username,
+		&user.HashedPassword,
+		&user.FullName,
+		&user.Email,
+		&user.PasswordChangedAt,
+		&user.CreatedAt,
+	)
+	return user, err
+}
+
+type UpdateUserParams struct {
+	HashedPassword    sql.NullString `json:"hashed_password"`
+	PasswordChangedAt sql.NullTime   `json:"password_changed_at"`
+	FullName          sql.NullString `json:"full_name"`
+	Email             sql.NullString `json:"email"`
+	Username          string         `json:"username"`
+}
+
+const updateUser = `
+UPDATE users
+SET
+    hashed_password = coalesce($1, hashed_password),
+    password_changed_at = coalesce($2, password_changed_at),
+    full_name = coalesce($3, full_name),
+    email = coalesce($4, email)
+WHERE username = $5
+RETURNING username, hashed_password, full_name, email, password_changed_at, created_at
+`
+
+func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, updateUser,
+		arg.HashedPassword,
+		arg.PasswordChangedAt,
+		arg.FullName,
+		arg.Email,
+		arg.Username,
+	)
 	var user User
 	err := row.Scan(
 		&user.Username,
