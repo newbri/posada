4ab894eb01eb961378963cf07139ba19d4c04055
Andy Newball <andynewball@gmail.com>
2023-11-30 20:03:36 -0500

    adding method to delete user

--- db/querier.go
+++ db/querier.go
@@ -6,4 +6,5 @@ type Querier interface {
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
 	GetUser(ctx context.Context, username string) (User, error)
 	UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error)
+	DeleteUser(ctx context.Context, username string) (User, error)
 }
