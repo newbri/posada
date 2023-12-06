1a4a59ea1aa96b7416f44e0d4806a3da3e8496ed
Andy Newball <andynewball@gmail.com>
2023-12-05 22:23:00 -0500

    Adding new methods in the interface

--- db/querier.go
+++ db/querier.go
@@ -1,10 +1,15 @@
 package db
 
-import "context"
+import (
+	"context"
+	"github.com/google/uuid"
+)
 
 type Querier interface {
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
 	GetUser(ctx context.Context, username string) (User, error)
 	UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error)
 	DeleteUser(ctx context.Context, username string) (User, error)
+	GetSession(ctx context.Context, id uuid.UUID) (Session, error)
+	CreateSession(ctx context.Context, arg CreateSessionParams) (Session, error)
 }
