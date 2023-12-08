bc644012a99645ec17468f7979e684a190a3c3ce
Andy Newball <andynewball@gmail.com>
2023-12-08 18:35:41 -0500

    creating method that will set the header with token

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,1 +1,19 @@
 package api
+
+import (
+	"fmt"
+	"github.com/newbri/posadamissportia/token"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func addAuthorization(t *testing.T, request *http.Request, tokenMaker token.Maker, authorizationType string, username string, duration time.Duration) {
+	token, payload, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	authorizationHeader := fmt.Sprintf("%s %s", authorizationType, token)
+	request.Header.Set(authorizationHeaderKey, authorizationHeader)
+}
