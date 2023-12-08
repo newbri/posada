bcaa7f16b6296402bf0d927676d42e3b2b477e46
Andy Newball <andynewball@gmail.com>
2023-12-08 18:36:41 -0500

    adding configuration to test

--- api/main_test.go
+++ api/main_test.go
@@ -4,13 +4,18 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/newbri/posadamissportia/db"
 	"github.com/newbri/posadamissportia/db/util"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
 )
 
 func newTestServer(t *testing.T, store db.Store) *Server {
-	config := util.Config{}
+	config, err := util.LoadConfig("../")
+	if err != nil {
+		log.Fatal().Msg("cannot load config")
+	}
+	//config := util.Config{}
 	server, err := NewServer(config, store)
 	require.NoError(t, err)
 
