ec23c5fea18510966b151ad96a55e556be8eb00b
Andy Newball <andynewball@gmail.com>
2023-12-14 18:32:28 -0500

    refactoring config structure to use yaml.v3

--- db/util/config.go
+++ db/util/config.go
@@ -1,34 +1,43 @@
 package util
 
 import (
-	"github.com/spf13/viper"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
 	"time"
 )
 
 type Config struct {
-	Environment          string        `mapstructure:"ENVIRONMENT"`
-	DBDriver             string        `mapstructure:"DB_DRIVER"`
-	DBSource             string        `mapstructure:"DB_SOURCE"`
-	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
-	HTTPServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
-	GRPCServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
-	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
-	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
-	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
-	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
+	Name                 string        `yaml:"name"`
+	DBDriver             string        `yaml:"db_driver"`
+	DBSource             string        `yaml:"db_source"`
+	MigrationURL         string        `yaml:"migration_url"`
+	HTTPServerAddress    string        `yaml:"http_server_address"`
+	TokenSymmetricKey    string        `yaml:"token_symmetric_key"`
+	AccessTokenDuration  time.Duration `yaml:"access_token_duration"`
+	RefreshTokenDuration time.Duration `yaml:"refresh_token_duration"`
+	RedisAddress         string        `yaml:"http_redis_address"`
 }
 
-func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
-	viper.SetConfigType("env")
-	viper.AutomaticEnv()
+type App struct {
+	Config map[string]*Config `yaml:"config"`
+}
 
-	err = viper.ReadInConfig()
+func LoadConfig(path string, env string) (*Config, error) {
+	file, err := os.ReadFile(path)
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	err = viper.Unmarshal(&config)
-	return
+	var app App
+	err = yaml.Unmarshal(file, &app)
+	if err != nil {
+		return nil, err
+	}
+
+	config, ok := app.Config[env]
+	if !ok {
+		return nil, fmt.Errorf("the environment does not exist")
+	}
+	return config, err
 }
