602ac757277de0d5200bf1b9c295d75720029a99
Andy Newball <andynewball@gmail.com>
2023-12-14 18:26:06 -0500

    changing type to pointer

--- api/server.go
+++ api/server.go
@@ -9,13 +9,13 @@ import (
 )
 
 type Server struct {
-	config     util.Config
+	config     *util.Config
 	store      db.Store
 	router     *gin.Engine
 	tokenMaker token.Maker
 }
 
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+func NewServer(config *util.Config, store db.Store) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker: %w", err)
