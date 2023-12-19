548cff2c458c0ed61475f2908b8e351520fe71c4
Andy Newball <andynewball@gmail.com>
2023-12-18 21:56:35 -0500

    creating role endpoint to create a role

--- api/role.go
+++ api/role.go
@@ -1,1 +1,45 @@
 package api
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+	"github.com/newbri/posadamissportia/db"
+	"net/http"
+)
+
+type createRoleRequest struct {
+	Name        string `json:"name" binding:"required,alphanum"`
+	Description string `json:"description"`
+}
+
+func (server *Server) createRole(ctx *gin.Context) {
+	var request createRoleRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateRoleParams{
+		ID:          uuid.New(),
+		Name:        request.Name,
+		Description: request.Description,
+	}
+
+	role, err := server.store.CreateRole(ctx, arg)
+	if err != nil {
+		var pqErr *pq.Error
+		if errors.As(err, &pqErr) {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, role)
+}
