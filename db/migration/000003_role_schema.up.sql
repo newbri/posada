23c47a0f34bf50cac143739e545e314d27b99a8e
Andy Newball <andynewball@gmail.com>
2023-12-18 21:47:28 -0500

    adding a default value to the column updated_at

--- db/migration/000003_role_schema.up.sql
+++ db/migration/000003_role_schema.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE "role"
     "id"            uuid PRIMARY KEY,
     "name"          text        NOT NULL,
     "description"   text        NOT NULL,
-    "updated_at"    timestamptz NOT NULL,
+    "updated_at"    timestamptz NOT NULL DEFAULT (now()),
     "created_at"    timestamptz NOT NULL DEFAULT (now())
 );
