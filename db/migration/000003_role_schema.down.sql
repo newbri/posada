fa9255fb9af80893b3f6e241d7e7bd790ace0c0e
Andy Newball <andynewball@gmail.com>
2023-12-21 13:42:39 -0500

    changing role schema

--- db/migration/000003_role_schema.down.sql
+++ db/migration/000003_role_schema.down.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS "role";
-DROP SEQUENCE IF EXISTS role_sequence;
+DROP SEQUENCE IF EXISTS "role_sequence";
+DROP INDEX IF EXISTS "external_id_index";
--- db/migration/000003_role_schema.up.sql
+++ db/migration/000003_role_schema.up.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS "role"
 (
-    "id"            uuid PRIMARY KEY,
-    "name"          text        NOT NULL,
-    "description"   text        NOT NULL,
-    "external_id"   text        NOT NULL UNIQUE,
-    "updated_at"    timestamptz NOT NULL DEFAULT (now()),
-    "created_at"    timestamptz NOT NULL DEFAULT (now())
+    "internal_id" uuid PRIMARY KEY,
+    "name"        text        NOT NULL,
+    "description" text        NOT NULL,
+    "external_id" text        NOT NULL UNIQUE,
+    "updated_at"  timestamptz NOT NULL DEFAULT (now()),
+    "created_at"  timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE SEQUENCE IF NOT EXISTS role_sequence START 101;
+CREATE SEQUENCE IF NOT EXISTS "role_sequence" START 101;
+CREATE UNIQUE INDEX IF NOT EXISTS "external_id_index" ON "role" ("external_id");
