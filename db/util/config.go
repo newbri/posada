4cd9f0c447cba3fb1d02cbbc41ef668205487299
Andy Newball <andynewball@gmail.com>
2024-01-30 11:31:57 -0500

    adding new fields

--- db/util/config.go
+++ db/util/config.go
@@ -8,16 +8,19 @@ import (
 )
 
 type Config struct {
-	Name                 string        `yaml:"name"`
-	DBDriver             string        `yaml:"db_driver"`
-	DBSource             string        `yaml:"db_source"`
-	MigrationURL         string        `yaml:"migration_url"`
-	HTTPServerAddress    string        `yaml:"http_server_address"`
-	TokenSymmetricKey    string        `yaml:"token_symmetric_key"`
-	AccessTokenDuration  time.Duration `yaml:"access_token_duration"`
-	RefreshTokenDuration time.Duration `yaml:"refresh_token_duration"`
-	RedisAddress         string        `yaml:"http_redis_address"`
-	DefaultRole          string        `yaml:"default_role"`
+	Name                    string        `yaml:"name"`
+	DBDriver                string        `yaml:"db_driver"`
+	DBSource                string        `yaml:"db_source"`
+	MigrationURL            string        `yaml:"migration_url"`
+	HTTPServerAddress       string        `yaml:"http_server_address"`
+	TokenSymmetricKey       string        `yaml:"token_symmetric_key"`
+	AccessTokenDuration     time.Duration `yaml:"access_token_duration"`
+	RefreshTokenDuration    time.Duration `yaml:"refresh_token_duration"`
+	RedisAddress            string        `yaml:"http_redis_address"`
+	DefaultRole             string        `yaml:"default_role"`
+	authorizationHeaderKey  string        `yaml:"authorization_header_key"`
+	authorizationTypeBearer string        `yaml:"authorization_type_bearer"`
+	authorizationPayloadKey string        `yaml:"authorization_payload_key"`
 }
 
 type App struct {
