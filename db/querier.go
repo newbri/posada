c5e7971757bd338b839076b7791e362798eed5f9
Andy Newball <andynewball@gmail.com>
2024-02-07 17:13:59 -0500

    changing method arg type to pointer

--- db/querier.go
+++ db/querier.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Querier interface {
-	CreateUser(ctx context.Context, arg CreateUserParams) (*User, error)
+	CreateUser(ctx context.Context, arg *CreateUserParams) (*User, error)
 	GetUser(ctx context.Context, username string) (*User, error)
 	UpdateUser(ctx context.Context, arg UpdateUserParams) (*User, error)
 	DeleteUser(ctx context.Context, username string) (*User, error)
