fe931f29d936ef325403ad93de88bd959267bdfe
Andy Newball <andynewball@gmail.com>
2024-02-07 14:30:27 -0500

    Creating a function to create user with role

--- db/main_test.go
+++ db/main_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
 	_ "github.com/lib/pq"
@@ -30,6 +31,38 @@ func TestMain(m *testing.M) {
 	}(db)
 
 	os.Exit(m.Run())
+}
+
+func createRandomUserWithRole(role string, isDeleted bool) *User {
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	if err != nil {
+		return nil
+	}
+
+	t := time.Now()
+	return &User{
+		Username:          util.RandomOwner(),
+		HashedPassword:    hashedPassword,
+		FullName:          fmt.Sprintf("%s %s", util.RandomOwner(), util.RandomOwner()),
+		Email:             util.RandomEmail(),
+		PasswordChangedAt: t,
+		CreatedAt:         t,
+		Role:              createRandomRole(role),
+		IsDeleted:         isDeleted,
+		DeletedAt:         time.Time{},
+	}
+}
+
+func createRandomRole(roleType string) *Role {
+	t := time.Now()
+	return &Role{
+		InternalID:  uuid.New(),
+		Name:        roleType,
+		Description: util.RandomString(16),
+		ExternalID:  fmt.Sprintf("URE%d", util.RandomInt(101, 999)),
+		UpdatedAt:   t,
+		CreatedAt:   t,
+	}
 }
 
 func createRandomUser(t *testing.T) User {
