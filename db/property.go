809c403a94716cc86721f2e31129d5e00cb3e342
Andy Newball <andynewball@gmail.com>
2024-03-18 22:57:13 -0400

    implimenting the method to create property

--- db/property.go
+++ db/property.go
@@ -1,9 +1,16 @@
 package db
 
 import (
+	"context"
 	"github.com/google/uuid"
 	"time"
 )
+
+const createPropertyQuery = `
+	INSERT INTO property(internal_id, external_id, name, address, state, country, postal_code, phone, email, expired_at, created_at) 
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+	RETURNING internal_id, external_id, name, address, state, country, postal_code, phone, email, expired_at, created_at;
+`
 
 type CreatePropertyParams struct {
 	InternalID uuid.UUID
@@ -17,4 +24,37 @@ type CreatePropertyParams struct {
 	Email      string
 	ExpiredAt  time.Time
 	CreatedAt  time.Time
+}
+
+func (q *Queries) CreateProperty(ctx context.Context, arg CreatePropertyParams) (*Property, error) {
+	row := q.db.QueryRowContext(
+		ctx,
+		createPropertyQuery,
+		arg.InternalID,
+		arg.ExternalID,
+		arg.Name,
+		arg.Address,
+		arg.State,
+		arg.Country,
+		arg.PostalCode,
+		arg.Phone,
+		arg.Email,
+		arg.ExpiredAt,
+		arg.CreatedAt,
+	)
+	var property Property
+	err := row.Scan(
+		&property.InternalID,
+		&property.ExternalID,
+		&property.Name,
+		&property.Address,
+		&property.State,
+		&property.Country,
+		&property.PostalCode,
+		&property.Phone,
+		&property.Email,
+		&property.ExpiredAt,
+		&property.CreatedAt,
+	)
+	return &property, err
 }
