60f11cf9fb2e916ac5a8560ee05ae6cb69b6e7c0
Andy Newball <andynewball@gmail.com>
2024-03-18 22:38:50 -0400

    Creating struct to create property

--- db/property.go
+++ db/property.go
@@ -1,1 +1,20 @@
 package db
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type CreatePropertyParams struct {
+	InternalID uuid.UUID
+	ExternalID string
+	Name       string
+	Address    string
+	State      string
+	Country    string
+	PostalCode string
+	Phone      string
+	Email      string
+	ExpiredAt  time.Time
+	CreatedAt  time.Time
+}
